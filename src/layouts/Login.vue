d4c959e34a5192ec4207bb1c5f2ea9e536a60b56
Iago Cavalcante <iagoangelimc@gmail.com>
2018-08-20 14:02:24 -0300

    Login pages

---
+++ src/layouts/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-layout view="lHh Lpr lFf" class="img-background" style="border: solid 1px black">
+   <q-page-container class="container-fluid">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style>
+::-webkit-scrollbar {
+display: none;
+}
+.img-background{
+  background: #fff url(https://images.unsplash.com/photo-1510511459019-5dda7724fd87?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=74450f7cf15b30e723c1a6d49abcc62c&auto=format&fit=crop&w=750&q=80);
+  /* Full height */
+  height: 100%;
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+
+.dark{
+  /* color: #fff !important; */
+  background: #42424236;
+  /* opacity: 0.3; */
+}
+</style>
---
+++ src/pages/auth/Login.vue
@@ -0,0 +1,53 @@
+<template>
+    <q-page  class="docs-input row justify-center ">
+        <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 col-xs-12 q-pa-xl dark">
+            <div class="text-center" style="color:white">
+            <p class="q-display-1 text-weight-bolder" colorName="negative">Dashboard Sample</p>
+            </div>
+
+            <div class="q-mt-md">
+            <q-input v-model="userData.email"  inverted color="dark" type="email" float-label="Login"
+            autocomplete="off" autocorrect="off" autocapitalize="off" :before="[{icon: 'person', handler () {}}]"  />
+            </div>
+
+            <div class="q-mt-md">
+            <q-input v-model="userData.password" inverted color="dark" type="password" float-label="Senha"
+            autocomplete="off" autocorrect="off" autocapitalize="off" :before="[{icon: 'lock', handler () {}}]" @keyup.enter="login(userData)" />
+            </div>
+
+            <!-- <div class="q-mt-md">
+            <q-btn color="white" flat rounded  class="full-width" label="Esqueci minha senha" size="sm" @click="recuperaSenha(userData)" ></q-btn>
+            </div> -->
+
+            <div class="q-mt-sm">
+            <q-btn color="negative" glossy class="full-width" label="Entrar" size="lg" @click="goToPage()" :loading="loading">
+            <span slot="loading">
+                <q-spinner-hourglass class="on-left" />
+                Carregando...
+            </span>
+            </q-btn>
+            </div>
+        </div>
+        </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      userData: {
+        email: 'user.test',
+        password: '12345678'
+      },
+      loading: false,
+      customDialogModel: false
+    }
+  },
+  methods: {
+    goToPage () {
+      this.$router.push('dashboard')
+    }
+  }
+}
+</script>
